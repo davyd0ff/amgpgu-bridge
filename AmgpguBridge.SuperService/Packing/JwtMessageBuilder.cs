c5d9f58510f2a0aa03d81eea356a7cbb3090d324
Давыдов Аркадий Александрович <davydovaa43.1@amgpgu.ru>
2022-06-09 10:50:01 +1000

    add jwtMessageBuilder

---
+++ AmgpguBridge.SuperService/Packing/JwtMessageBuilder.cs
@@ -0,0 +1,46 @@
+﻿using AmgpguBridge.SuperService.Encoding;
+using AmgpguBridge.SuperService.Signing;
+
+namespace AmgpguBridge.SuperService.Packing;
+
+public class JwtMessageBuilder
+{
+  private readonly JwtMessage _jwtMessage;
+  private bool _signNeedly = true;
+
+  public JwtMessageBuilder(ISigner signer, IEncoder encoder)
+  {
+    this._jwtMessage = new JwtMessage(signer, encoder);
+  }
+
+  public JwtMessageBuilder SetHeader(string header)
+  {
+    this._jwtMessage.SetHeader(header);
+    this._signNeedly = true;
+    return this;
+  }
+
+  public JwtMessageBuilder SetPayload(string payload)
+  {
+    this._jwtMessage.SetPayload(payload);
+    this._signNeedly = true;
+    return this;
+  }
+
+  public JwtMessageBuilder SetEncodedJwtMessage(string encodedJwtMessage)
+  {
+    this._jwtMessage.CreateByEncodedJwtMessage(encodedJwtMessage);
+    this._signNeedly = false;
+    return this;
+  }
+
+  public JwtMessage GetResult()
+  {
+    if (this._signNeedly)
+    {
+      this._jwtMessage.Sign();
+    }
+
+    return this._jwtMessage;
+  }
+}
