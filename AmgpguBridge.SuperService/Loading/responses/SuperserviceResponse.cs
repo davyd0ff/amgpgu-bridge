904f3c20afbf24c63c74ed7d7b990e2e5b31661e
Давыдов Аркадий Александрович <davydovaa43.1@amgpgu.ru>
2022-06-09 10:52:13 +1000

    add model response from SuperService

---
+++ AmgpguBridge.SuperService/Loading/responses/SuperserviceResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace AmgpguBridge.SuperService.Loading;
+
+public class SuperserviceResponse
+{
+  public string IdJwt { get; set; }
+  public string ResponseToken { get; set; }
+  public string Error { get; set; }
+  public string Result { get; set; }
+  public HttpRequestException Exception { get; set; }
+
+  public SuperserviceResponse() { }
+  public SuperserviceResponse(HttpRequestException exception) { 
+    this.Exception = exception;
+  }
+}
