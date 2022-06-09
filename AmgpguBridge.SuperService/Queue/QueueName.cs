f58abebfa4bba3ed2cd3c65f48e2f8e08daa4237
Давыдов Аркадий Александрович <davydovaa43.1@amgpgu.ru>
2022-06-09 16:04:44 +1000

    queue name

---
+++ AmgpguBridge.SuperService/Queue/QueueName.cs
@@ -0,0 +1,22 @@
+﻿namespace AmgpguBridge.SuperService.Queue;
+
+public enum QueueName
+{
+  Database,
+
+  UniversityApplicationNew,
+  UniversityApplicationChange,
+  UniversityApplicationReject,
+
+  UniversityPlanNew,
+  UniversityPlanChange,
+
+  UniversityOrderEnroll,
+  UniversityOrderUnenroll,
+
+  SuperServiceApplicationNew,
+  SuperServiceApplicationChange,
+  SuperServiceApplicationReject,
+  SuperServiceGetMessageInfo,
+  SuperServiceMessageConfirm,
+}
