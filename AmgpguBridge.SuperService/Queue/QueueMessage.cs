d986a445581029332ec7660439126b2fa9307d45
Давыдов Аркадий Александрович <davydovaa43.1@amgpgu.ru>
2022-06-09 16:03:11 +1000

    add model queue message

---
+++ AmgpguBridge.SuperService/Queue/QueueMessage.cs
@@ -0,0 +1,13 @@
+﻿namespace AmgpguBridge.SuperService.Queue;
+
+public class QueueMessage
+{
+  public string Body { get; }
+  public string UidMessage { get; }
+  public string UidEntity { get; }
+  public DateTime Date { get; }
+  public QueueName QueueName { get; }
+  public QueueMessage ParentMessage { get; set; }
+  public QueueMessageStatus Status { get; set; }
+  public string Error { get; set; }
+}
