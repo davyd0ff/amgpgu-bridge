9603aec5148b545bc7fd94b5d4eb921c59e544ff
Давыдов Аркадий Александрович <davydovaa43.1@amgpgu.ru>
2022-06-10 13:44:20 +1000

    implemention IMapper, based on Automapper

---
+++ AmgpguBridge.SuperService/Mapping/Mapper.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+
+namespace AmgpguBridge.SuperService.Mapping;
+
+public class Mapper : IMapper
+{
+  private AutoMapper.Mapper _mapper;
+
+  public Mapper()
+  {
+    var configuration = new MapperConfiguration(cfg =>
+    {
+      cfg.AddMaps("AmgpguBridge.SuperService.Mapping.Profiles");
+    });
+
+    this._mapper = new AutoMapper.Mapper(configuration);
+  }
+
+  public TSEntity Map<TUEntity, TSEntity>(TUEntity entity)
+  {
+    return this._mapper.Map<TUEntity, TSEntity>(entity);
+  }
+}
