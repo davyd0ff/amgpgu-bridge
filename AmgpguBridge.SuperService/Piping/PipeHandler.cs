f365c45dcdb853bd3a359fa5fb81cb87eaf95b25
Давыдов Аркадий Александрович <davydovaa43.1@amgpgu.ru>
2022-06-23 16:32:10 +1000

    add pipe as CoR

---
+++ AmgpguBridge.SuperService/Piping/PipeHandler.cs
@@ -0,0 +1,27 @@
+﻿namespace AmgpguBridge.SuperService.Piping;
+
+public abstract class PipeHandler
+{
+  private PipeHandler _next = null;
+
+  public void SetNextHandler(PipeHandler next)
+  {
+    if (this._next == null)
+    {
+      this._next = next;
+    }
+    else
+    {
+      this._next.SetNextHandler(next);
+    }
+  }
+
+  public virtual void Handle<TSEntity>(Context<TSEntity> context) 
+    where TSEntity : Entities.SuperService.Entity
+  {
+    if (this._next != null)
+    {
+      this._next.Handle(context);
+    }
+  }
+}
---
+++ AmgpguBridge.SuperService/Piping/Pipeline.cs
@@ -0,0 +1,28 @@
+﻿namespace AmgpguBridge.SuperService.Piping;
+
+public class Pipeline<TSEntity> where TSEntity : Entities.SuperService.Entity
+{
+  private PipeHandler _handler = null;
+
+  public readonly IServiceProvider ServiceProvider;
+  public readonly Context<TSEntity> Context;
+
+
+
+  public Pipeline(Context<TSEntity> context, IServiceProvider serviceProvider)
+  {
+    this.Context = context;
+    this.ServiceProvider = serviceProvider;
+  }
+
+
+  public void AddHandler(PipeHandler handler)
+  {
+    this._handler.SetNextHandler(handler);
+  }
+
+  public void Handle()
+  {
+    this._handler.Handle(this.Context);
+  }
+}
