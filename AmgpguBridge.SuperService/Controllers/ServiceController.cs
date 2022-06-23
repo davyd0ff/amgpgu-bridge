eb15dde39f8f6a58dad5d85a5121712b5f82f590
Давыдов Аркадий Александрович <davydovaa43.1@amgpgu.ru>
2022-06-23 16:30:10 +1000

    Add Service Controller which performs the exchange of service messages

---
+++ AmgpguBridge.SuperService/Controllers/ServiceController.cs
@@ -0,0 +1,34 @@
+﻿using AmgpguBridge.SuperService.Entities.SuperService;
+using AmgpguBridge.SuperService.Piping;
+using AmgpguBridge.SuperService.Queue;
+
+namespace AmgpguBridge.SuperService.Controllers;
+
+public class ServiceController
+{
+  private readonly IServiceProvider ServiceProvider;
+  public ServiceController(IServiceProvider serviceProvider)
+  {
+    ServiceProvider = serviceProvider;
+  }
+
+  public async Task GetInfo(QueueMessage queueMessage)
+  {
+    new Pipeline<EmptyEntity>(new Context<EmptyEntity>(queueMessage, SuperServiceAction.GetMessage), ServiceProvider)
+      .Error()
+      .ItIsInDatabase()
+      .Pack()
+      .Load("api/token/service/info")
+      .Move();
+  }
+
+  public async Task Confirm(QueueMessage queueMessage)
+  {
+    new Pipeline<EmptyEntity>(new Context<EmptyEntity>(queueMessage, SuperServiceAction.Confirm), ServiceProvider)
+      .Error()
+      .ItIsInDatabase()
+      .Pack()
+      .Load("api/token/service/confirm")
+      .Move();
+  }
+}
