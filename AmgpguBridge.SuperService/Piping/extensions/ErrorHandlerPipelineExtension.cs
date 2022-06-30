78134ac6b47bcde0e7a6bbb9a147a5192501d139
Давыдов Аркадий Александрович <davydovaa43.1@amgpgu.ru>
2022-06-30 17:13:46 +1000

    extension for pipeline which added ErrorHandler

---
+++ AmgpguBridge.SuperService/Piping/extensions/ErrorHandlerPipelineExtension.cs
@@ -0,0 +1,17 @@
+﻿using AmgpguBridge.SuperService.Queue.QeueMoveStrategies;
+
+namespace AmgpguBridge.SuperService.Piping;
+
+public static class ErrorHandlerPipelineExtension
+{
+  public static Pipeline Error(this Pipeline pipeline)
+  {
+    var queueMoveStrategyFactory = pipeline.ServiceProvider.GetRequiredService<QueueMoveStrategyFactory>();
+
+    pipeline.AddHandler(
+      new ErrorHandler(queueMoveStrategyFactory)
+    );
+
+    return pipeline;
+  }
+}
