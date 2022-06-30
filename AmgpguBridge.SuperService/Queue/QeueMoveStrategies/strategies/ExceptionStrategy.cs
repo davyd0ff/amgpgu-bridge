4bb7b5f147daedb4578c711244edceebb2dad0b1
Давыдов Аркадий Александрович <davydovaa43.1@amgpgu.ru>
2022-06-30 17:11:18 +1000

    Add strategy how to move queuemessage when exception rises

---
+++ AmgpguBridge.SuperService/Queue/QeueMoveStrategies/strategies/ExceptionStrategy.cs
@@ -0,0 +1,21 @@
+﻿namespace AmgpguBridge.SuperService.Queue.QeueMoveStrategies;
+
+public class ExceptionStrategy : IQueueMoveStrategy
+{
+  private IQueueWriter _queueWriter;
+  private readonly Exception _exception;
+
+  public ExceptionStrategy(IQueueWriter queueWriter, Exception exception)
+  {
+    this._queueWriter = queueWriter;
+    this._exception = exception;
+  }
+
+  public void MoveQueueMessage(QueueMessage queueMessage)
+  {
+    queueMessage.Status = QueueMessageStatus.Error;
+    queueMessage.Error = this._exception.ToString();
+
+    this._queueWriter.Write(QueueName.Database, queueMessage);
+  }
+}
